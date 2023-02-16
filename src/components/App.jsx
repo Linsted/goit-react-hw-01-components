73bbda0f08366c8c455a5e0e93a478d9ca109816
Ivan Malenko <ivan.malenko18@gmail.com>
2023-02-16 15:51:44 +0200

    finished 2nd task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,11 @@
 import { Profile } from '../components/Profile/Profile'
-
+import {Statistics} from "components/Statistics/Statistics"
 
 export const App = () => {
   return (
+    <>
     < Profile />
+    <Statistics />
+    </>
     );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,19 +1,28 @@
-import { ProfileDescription } from "components/ProfileDescription/ProfileDescription";
+import { ProfileDescription } from "components/Profile/ProfileDescription/ProfileDescription";
 import user from "user.json";
-import {ProfileStatistics} from 'components/ProfileStatistics/ProfileStatistics' 
+import {ProfileStatistics} from 'components/Profile/ProfileStatistics/ProfileStatistics' 
 import css from "./Profile.module.css"
 
 
 
 
 export const Profile = () => {
-
+    // console.log(user.username)
     return (
     <div className={css.profile}>
-            <ProfileDescription user={user} />
+            <ProfileDescription 
+                username={user.username}
+                avatar={user.avatar}
+                tag={user.tag}
+                location={user.location}
+            />
 
 
-            <ProfileStatistics user={user} />
+            <ProfileStatistics 
+                followers={user.stats.followers}
+                views={user.stats.views}
+                likes={user.stats.likes}
+            />
     </div>
     )
 }
