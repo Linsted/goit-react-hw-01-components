f39c2966201e2054f3796fd9de0722c07f372570
Ivan Malenko <ivan.malenko18@gmail.com>
2023-02-17 11:36:47 +0200

    fixed task 4

--- src/components/TransactionHistory/TransactionHistoryTableBody/TransactionHistoryTableBody.jsx
+++ src/components/TransactionHistory/TransactionHistoryTableBody/TransactionHistoryTableBody.jsx
@@ -6,15 +6,20 @@ export const TransactionHistoryTableBody = ({transactions}) => {
     console.log(transactions)
     return (
         <tbody >
-            {transactions.map(transaction => <tr>
-      <td>Invoice</td>
-      <td>125</td>
-      <td>USD</td>
+            {transactions.map(transaction => <tr key={transaction.id}>
+      <td>{transaction.type}</td>
+      <td>{transaction.amount}</td>
+      <td>{transaction.currency}</td>
     </tr>)}
         </tbody>
     )
 }
 
-// TransactionHistoryTableBody.propTypes = {
-//     transactions: PropTypes.arrayOf().isRequired,
-// }
+TransactionHistoryTableBody.propTypes = {
+    transactions: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        amount: PropTypes.string.isRequired,
+        currency: PropTypes.string.isRequired,
+    }).isRequired,).isRequired,
+}
